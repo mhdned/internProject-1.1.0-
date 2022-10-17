000426c7497ab754a544b48b8f36112bb5f58160
Mehtiuo <mahdi.nedaee4@gmail.com>
2022-10-17 12:45:22 +0330

    nothing

--- utils/dateHandler.js
+++ utils/dateHandler.js
@@ -1,5 +1,6 @@
 const moment = require('jalali-moment-timezone');
 
 exports.dateToString = (date) => {
-    return moment(date,'X').format("jYYYY/jMM/jDD (hh::mm::ss)");
+    return moment(date,'X').format("jYYYY/jMM/jDD (hh:mm:ss)");
 }
+exports.dateToNumber = (date) => {}
